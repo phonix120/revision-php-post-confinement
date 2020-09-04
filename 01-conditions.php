9a94e5202996a959c1511741e89adaf1b95b8628
phonix120 <web2020.lorenzo@gmail.com>
2020-09-04 16:06:21 +0200

    exercice 2

--- 01-conditions.php
+++ 01-conditions.php
@@ -41,6 +41,12 @@ if($hasard<5){
 }
 
 ?>
+<h2>Condition ternaire: peut faire ce que fait un if / else en une ligne seulement</h2>
+<?php
+// faites une ternaire sur hasard avec les résultats de l'exemple précédent mise dans une variable, la condition est que $hasard est ou plus petit que 5 ou plus grand ou égal à 5
+$ternaire=($hasard<5)? $hasard." est plus petit que 5": $hasard." est plus grand ou égal à 5";
+echo $ternaire;
+?>
 <p>On peut imbriquer les conditions if else, en général on évite d'avoir plus de 3 niveaux d'imbrication</p>
 <h5>
 <?php
@@ -74,17 +80,30 @@ if($hasard<5){
 }
 ?>
 </h5>
-<h3>Explication sur le modulo</h3>
-<p>le modulo d'un chiffre nous donne le nombre d'éléments derrière la virgule après la division de celui-ci</p>
-<hr>
+<h1>condition sinon si: elseif ou else if</h1>
+<p>On verifie si 1 condition est remplie SINON SI (elseif) on vérifie une autre condition, SINON on effectue une action par défaut</p>
 <?php
-$a = 8;
-$b = $a%2; // on divise par 2 et on regarde si il y a quelque chose après la virgule
-$c = $a%3; // on divise 2 par 3 et on regarde combien on a d'éléments dans le cas de 8 / 3, on peut 2x 3 dedans = 6, il faut rajouter 2 pour arriver à 8
-echo $b." | ".$c;
-echo "<br>".($a/3);
+
+// SI on va vérifier si le chiffre au hasard est plus petit que 5 et qu'il est impaire
+if($hasard<5 && $hasard%2){
+    echo '$hasard vaut '."$hasard et est donc plus petit que 5 et est impaire";
+
+// SINON SI hasard est plus petit que 5 et qu'il est paire !() => inverse d'impaire
+}else if($hasard<5 && !($hasard%2) ){
+    echo '$hasard vaut '."$hasard et est donc plus petit que 5 et est paire";
+
+// SINON SI on va vérifier si le chiffre au hasard est plus grand ou égal à 5 et qu'il est impaire
+}elseif($hasard>=5 && $hasard%2){
+
+    echo '$hasard vaut '. "$hasard et est donc plus grand ou égal à 5 et est impaire";
+// SINON tout les cas de figures possibles on été listés précédemment, le else peut donc nous afficher la dernière option possible
+}else{
+    echo '$hasard vaut '. "$hasard et est donc plus grand ou égal à 5 et est paire";
+}
+
 ?>
-<hr>
+
+
 </body>
 </html>
 
